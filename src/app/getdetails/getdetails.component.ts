0177b17bccf597645f13b837ce02acb5ae32f3bc
steeliz <mathewrabeen@gmail.com>
2020-03-20 11:27:06 +0530

    added confirm password and disabled save

--- src/app/getdetails/getdetails.component.ts
+++ src/app/getdetails/getdetails.component.ts
@@ -8,10 +8,12 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class GetdetailsComponent implements OnInit {
 
-  @Output() formSave = new EventEmitter <{name:string, password:string}> ();
+  showSaveButton=false;
+
+  @Output() formSave = new EventEmitter <{name:string, password:string,confirmPassword:string}> ();
 
 
-formData:{name:string, password:string} = {name:"", password:""};
+formData:{name:string, password:string,confirmPassword:string} = {name:"", password:"",confirmPassword:""};
 // for two-way binding
 
   constructor() { }
@@ -22,6 +24,15 @@ formData:{name:string, password:string} = {name:"", password:""};
   handleSave() {
     this.formSave.emit({...this.formData});
     //spread operator is used to create a new copy of object in order to avoid aliasing issue.
+}
+
+confirmPasswordCheck() {
+  if ( this.formData.password === this.formData.confirmPassword){
+this.showSaveButton=true;
+  }
+  else{
+    this.showSaveButton=false;
+  }
 }
 
 }
